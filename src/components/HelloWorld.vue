5ec70a444932fcd5866eb81cde196026080d33c4
JimmyVazz <cjvasquez_98@hotmail.com>
2020-02-15 17:09:32 -0600

    primer commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <v-app-bar> 
+    <v-toolbar-title>
+      Arca de Noe
+    </v-toolbar-title>
+    <v-spacer/>
+  <v-btn
+  icon
+  @click="getWeb3"
+  >
+  <v-icon color="purple">
+    mdi-account
+    </v-icon>
+  </v-btn>
+    {{this.cuentas ? this.cuentas[0] : ""}}
+    <v-btn
+  icon
+  >
+  <v-icon color="purple">
+    mdi-wallet
+    </v-icon>
+    </v-btn>
+
+  </v-app-bar> 
+  
+  </div>
+</template>
+
+<script>
+import Web3 from 'web3';
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      cuentas: null,
+      balance: null,
+    }),
+      methods: {
+        async getWeb3(){
+          await window.ethereum.enable();
+          const instance = new Web3(window.ethereum);
+          this.cuentas = await instance.eth.getAccounts();
+          window.ethereum.on('accountsChanged', (nuevas_cuentas) => {
+            this.cuentas = nuevas_cuentas
+            console.log(this.cuentas)
+          })
+          
+        },
+        mounted(){
+          
+        }
+        
+
+
+      }
+  }
+</script>
